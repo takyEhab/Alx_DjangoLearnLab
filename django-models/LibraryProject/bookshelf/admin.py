230e4958ca95168c7d4e6102d24c978f3ef92080
takyEhab <tkyehab@gmail.com>
2025-02-26 14:48:50 +0200

    week10

---
+++ django-models/LibraryProject/bookshelf/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Book
+
+# Register your models here.
+# admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date')
+    list_filter = ('title', 'author', 'published_date')
+    search_fields = ('title', 'author')
+
+admin.site.register(Book, BookAdmin)
