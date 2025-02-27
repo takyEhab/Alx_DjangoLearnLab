0585f903af3e2bfc1cd34dfd0da9d6ed3b313b30
takyEhab <tkyehab@gmail.com>
2025-02-27 13:16:19 +0200

    week10

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,3 +2,10 @@
 
 books = Library.objects.get(name=library_name)
 books.all()
+
+author = Author.objects.get(name=author_name)
+objects.filter(author=author)
+
+librarian = Librarian.objects.get(name=librarian_name)
+
+books.libraries.add(librarian.library)
