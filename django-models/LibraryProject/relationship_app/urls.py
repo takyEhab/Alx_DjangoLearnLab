6a86f944396423516f04f44e081c62a92e4b43a9
takyEhab <tkyehab@gmail.com>
2025-02-28 15:15:55 +0200

    chatGPT

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import book_list, LibraryDetailView
+
+
+urlpatterns = [
+    path('books/', book_list, name='book_list'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+
+]
+
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Book, Library
+from django.views.generic import DetailView
 
-# Create your views here.
+def book_list(request):
+    books = Book.objects.all()  
+    return render(request, 'books/book_list.html', {'books': books})
+
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.books.all()  
+        return context
