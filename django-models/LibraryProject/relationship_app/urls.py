3985a390817172b5a1db23912a677591ecc23cc5
takyEhab <tkyehab@gmail.com>
2025-02-28 15:29:50 +0200

    chatGPT

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import book_list, LibraryDetailView
-
+from .views import LibraryDetailView
+from .views import list_books
 
 urlpatterns = [
-    path('books/', book_list, name='book_list'),
+    path('books/', list_books, name='book_list'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,7 @@ from .models import Book
 from django.views.generic.detail import DetailView
 from .models import Library 
 
-def book_list(request):
+def list_books(request):
     books = Book.objects.all()  
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
