939ceabd8c3ceae926054e6492afedf3c1a28bd5
takyEhab <tkyehab@gmail.com>
2025-02-28 15:18:21 +0200

    chatGPT

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,7 @@ from django.views.generic import DetailView
 
 def book_list(request):
     books = Book.objects.all()  
-    return render(request, 'books/book_list.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 
 class LibraryDetailView(DetailView):
