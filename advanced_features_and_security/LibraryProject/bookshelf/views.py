c2c4f53e52ebef0d31e71a616bfd24197ae17434
takyEhab <tkyehab@gmail.com>
2025-03-14 07:20:21 +0200

    task2

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -37,3 +37,44 @@ def book_delete(request, book_id):
         book.delete()
         return redirect("book_list")
     return render(request, "bookshelf/book_confirm_delete.html", {"book": book})
+
+
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required, permission_required
+from .models import Book
+from .forms import BookForm  # Use Django forms to validate input
+
+@login_required
+@permission_required("bookshelf.can_view", raise_exception=True)
+def book_list(request):
+    query = request.GET.get("q")
+    if query:
+        books = Book.objects.filter(title__icontains=query)  # Prevents SQL injection
+    else:
+        books = Book.objects.all()
+    return render(request, "bookshelf/book_list.html", {"books": books})
+
+@login_required
+@permission_required("bookshelf.can_create", raise_exception=True)
+def book_create(request):
+    if request.method == "POST":
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("book_list")
+    else:
+        form = BookForm()
+    return render(request, "bookshelf/book_form.html", {"form": form})
+
+@login_required
+@permission_required("bookshelf.can_edit", raise_exception=True)
+def book_edit(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == "POST":
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect("book_list")
+    else:
+        form = BookForm(instance=book)
+    return render(request, "bookshelf/book_form.html", {"form": form, "book": book})
