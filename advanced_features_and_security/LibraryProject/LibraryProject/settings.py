ac6b7dc4d2b96123b75bb0b91eda675803632b78
takyEhab <tkyehab@gmail.com>
2025-03-14 07:09:30 +0200

    task0

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -127,6 +127,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
-AUTH_USER_MODEL = "relationship_app.CustomUser"
+AUTH_USER_MODEL = "bookshelf.CustomUser"
 # AUTH_USER_MODEL = "relationship_app.customuser"
 
--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Book
 
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
 # Register your models here.
 # admin.site.register(Book)
 class BookAdmin(admin.ModelAdmin):
@@ -9,3 +13,24 @@ class BookAdmin(admin.ModelAdmin):
     search_fields = ('title', 'author')
 
 admin.site.register(Book, BookAdmin)
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("username", "email", "date_of_birth", "is_staff", "is_superuser")
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        ("Personal Info", {"fields": ("date_of_birth", "profile_photo")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
+        ("Important Dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "email", "date_of_birth", "password1", "password2"),
+        }),
+    )
+    search_fields = ("email", "username")
+    ordering = ("email",)
+
+admin.site.register(CustomUser, CustomUserAdmin)
