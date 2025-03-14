aafb926e9eebac9128ebea7921503d3cac0fded2
takyEhab <tkyehab@gmail.com>
2025-03-14 07:23:36 +0200

    task2

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -42,7 +42,7 @@ def book_delete(request, book_id):
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required, permission_required
 from .models import Book
-from .forms import BookForm  # Use Django forms to validate input
+from .forms import ExampleForm  # Use Django forms to validate input
 
 @login_required
 @permission_required("bookshelf.can_view", raise_exception=True)
@@ -77,4 +77,49 @@ def book_edit(request, book_id):
             return redirect("book_list")
     else:
         form = BookForm(instance=book)
+    return render(request, "bookshelf/book_form.html", {"form": form, "book": book})
+
+
+
+
+
+
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required, permission_required
+from .models import Book
+from .forms import ExampleForm  # Use Django forms to validate input
+
+@login_required
+@permission_required("bookshelf.can_view", raise_exception=True)
+def book_list(request):
+    query = request.GET.get("q")
+    if query:
+        books = Book.objects.filter(title__icontains=query)  # Prevents SQL injection
+    else:
+        books = Book.objects.all()
+    return render(request, "bookshelf/book_list.html", {"books": books})
+
+@login_required
+@permission_required("bookshelf.can_create", raise_exception=True)
+def book_create(request):
+    if request.method == "POST":
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("book_list")
+    else:
+        form = ExampleForm()
+    return render(request, "bookshelf/book_form.html", {"form": form})
+
+@login_required
+@permission_required("bookshelf.can_edit", raise_exception=True)
+def book_edit(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == "POST":
+        form = ExampleForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect("book_list")
+    else:
+        form = ExampleForm(instance=book)
     return render(request, "bookshelf/book_form.html", {"form": form, "book": book})
