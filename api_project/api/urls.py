c14545245b45286c14e47a0e3804b687a2b18114
takyEhab <tkyehab@gmail.com>
2025-04-06 05:39:30 +0200

    third api commit

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,5 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from .views import BookList
-urlpatterns = [
+from .views import BookViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
+
+urlpatterns = [    
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
+    path('', include(router.urls)),  # This includes all routes registered with the router
+
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework import viewsets
 from .serializers import BookSerializer
 from .models import Book
 
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
