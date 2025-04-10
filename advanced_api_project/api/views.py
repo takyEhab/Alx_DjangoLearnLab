9514c2f89ba14e26604d66988cbe8c0142953b89
takyEhab <tkyehab@gmail.com>
2025-04-10 13:57:33 +0200

    first taksk week13

--- advanced_api_project/api/views.py
+++ advanced_api_project/api/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+class BookListView(generics.ListAPIView):
+    """
+    Retrieves a list of all books.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDetailView(generics.RetrieveAPIView):
+    """
+    Retrieves a single book by ID.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookCreateView(generics.CreateAPIView):
+    """
+    Creates a new book entry.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookUpdateView(generics.UpdateAPIView):
+    """
+    Updates an existing book entry.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDeleteView(generics.DestroyAPIView):
+    """
+    Deletes an existing book entry.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
